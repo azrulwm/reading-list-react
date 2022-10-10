5c5751822e47c6952f090e485a79e23425971c78
azrulwm <azrulazmi.wm@gmail.com>
2022-10-10 17:56:58 +0800

    uploading basic react files

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Reading list App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;

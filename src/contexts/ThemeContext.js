1b01307b068a1dc544c1afeeef163806880b515c
azrulwm <azrulazmi.wm@gmail.com>
2022-10-10 18:47:22 +0800

    creating context folder and ThemeContext.js

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeContextProvider = ({ children }) => {
+  const state = {
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  };
+  return (
+    <ThemeContext.Provider value={{ ...state }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
